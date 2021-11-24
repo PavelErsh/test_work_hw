204e5658d43c8652821254df009eb622a0c73cb6
PavelErsh <pavel204812@gmail.com>
2021-11-24 14:47:15 +0200

    add final sort

--- main.py
+++ main.py
@@ -7,9 +7,40 @@ SCREEN_HEIGHT = 600
 
 SCREEN_TITLE = "Шаблон"
 
+
+def merge_two_lists( a, b):
+    c = []
+    i = j = 0
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            c.append(a[i])
+            i += 1
+        else:
+            c.append(b[j])
+            j += 1
+
+    if i < len(a):
+        c += a[i:]
+
+    if j < len(b):
+        c += b[j:]
+
+    return c
+
+
+def merge_sort( s):
+    if len(s) == 1:
+        return s
+    middle = len(s) // 2
+    left = merge_sort(s[:middle])
+    right = merge_sort(s[middle:])
+    return merge_two_lists(left, right)
+
+
 class Rectangle(arcade.Sprite):
     def update(self):
-        pass
+        game.on_draw()
+
 
 class Text:
     pass
@@ -30,6 +61,12 @@ class OurGame(arcade.Window):
         self.text.center_y = []
         self.text.change_x = 0
 
+        for j in range(20):
+            self.rand_numbers.append(random.randint(1, 10))
+
+
+    # отрисовка объектов
+    def on_draw(self):
         for i in range(20):
             self.margin = 30
             self.position = 400
@@ -40,27 +77,21 @@ class OurGame(arcade.Window):
             self.text.center_y.append(rectange.center_y-4)
             self.rectangles.append(rectange)
 
-        for j in range(20):
-            self.rand_numbers.append(random.randint(1, 10))
-    # отрисовка объектов
-    def on_draw(self):
         arcade.start_render()
         arcade.set_background_color(arcade.color.WHITE)
         self.rectangles.draw()
         for i in range(20):
             arcade.draw_text(f"{self.rand_numbers[i]}",self.text.center_x[i], self.text.center_y[i], arcade.color.BLACK, 10 )
 
+        sort_arr = merge_sort(self.rand_numbers)
+
+        for i in range(20):
+            arcade.draw_text(f"{sort_arr[i]}",self.text.center_x[i], self.text.center_y[i]-60, arcade.color.BLACK, 10 )
+
     # логика
+    def drow_array(self):
+
     def update(self, delta_time):
-        pass
-        #self.text.center_y += self.text.change_x
-
-    # нажать на клавишу
-    def on_key_press(self, key, modifiers):
-        pass
-
-    # отпустить клавишу
-    def on_key_release(self, key, modifiers):
         pass
 
 
